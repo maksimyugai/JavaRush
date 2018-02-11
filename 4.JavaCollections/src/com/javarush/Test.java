f34b1c8b229b9abfa708b3426a4f7bcfd2bab06f
Maksim Yugai <maksim.yugai@gmail.com>
2018-02-12 01:12:01 +0600

    new tasks resolved

--- 4.JavaCollections/src/com/javarush/Test.java
+++ 4.JavaCollections/src/com/javarush/Test.java
@@ -1,16 +1,33 @@
 package com.javarush;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Test {
+    //текст для хеширования
+    public static final String TEXT = "Prologistic.com.ua";
 
-    public static void main(String[] args) throws IOException {
-        String content = new String(Files.readAllBytes(
-                Paths.get("~/Dropbox/Development/Java/JavaRush/JavaRushTasks/out.log")));
+    public static void main(String[] args) throws NoSuchAlgorithmException {
 
-        System.out.println(content);
-        
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(TEXT.getBytes());
+
+        byte byteData[] = md.digest();
+
+        //конвертируем байт в шестнадцатеричный формат первым способом
+        StringBuffer sb = new StringBuffer();
+        for (byte aByteData : byteData) {
+            sb.append(Integer.toString((aByteData & 0xff) + 0x100, 16).substring(1));
+        }
+        System.out.println("Текст в шестнадцатеричном виде : " + sb.toString());
+
+        //конвертируем байт в шестнадцатеричный формат вторым способом
+        StringBuffer hexString = new StringBuffer();
+        for (byte aByteData : byteData) {
+            String hex = Integer.toHexString(0xff & aByteData);
+            if (hex.length() == 1) hexString.append('0');
+            hexString.append(hex);
+        }
+        System.out.println("Текст в шестнадцатеричном виде : " + hexString.toString());
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task32/task3204/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task32/task3204/Solution.java
@@ -1,9 +1,21 @@
 package com.javarush.task.task32.task3204;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Random;
 
 /* 
 Генератор паролей
+
+Реализуй логику метода getPassword, который должен возвращать ByteArrayOutputStream, в котором будут байты пароля.
+Требования к паролю:
+1) 8 символов.
+2) только цифры и латинские буквы разного регистра.
+3) обязательно должны присутствовать цифры, и буквы разного регистра.
+Все сгенерированные пароли должны быть уникальные.
+
+Пример правильного пароля:
+wMh7smNu
 */
 public class Solution {
     public static void main(String[] args) {
@@ -12,6 +24,24 @@ public class Solution {
     }
 
     public static ByteArrayOutputStream getPassword() {
-        return null;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        Random r = new Random();
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        String result = String.valueOf(alphabet.substring(0, 27).charAt(r.nextInt(26)));
+        result += String.valueOf(alphabet.substring(27, 53).charAt(r.nextInt(26)));
+        result += String.valueOf(alphabet.substring(52).charAt(r.nextInt(10)));
+
+        for (int i = 0; i < 5; i++) {
+            result += alphabet.charAt(r.nextInt(alphabet.length()));
+        }
+
+        try {
+            outputStream.write(result.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return outputStream;
     }
 }
